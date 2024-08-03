2f06aa0882f622a4beedc57b4ff95d6d6cc78995
Baejita <homokoro@gmail.com>
2024-08-04 00:08:40 +0700

    complety React

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -5,10 +5,6 @@ function PostList() {
   const posts = useLoaderData();
   //เราไม่มสามารถสร้าง asyn await ตรงฟังชั่นหลักได้ ต้องใช้ useEffect ในการห่อ
 
-  function addPostHandler(postData) {
-   
-    setPosts((existingPosts) => [postData, ...existingPosts]);
-  }
   //ใช้ exsting => ในกรณีที่ขึ้นอยู่กับสถานะเก่า
 
   //มีอยู่สามวิธีในการสร้างเงื่อนไขคึือ 1 if 2. tertinary operations 3. ใช้ &&
@@ -16,8 +12,13 @@ function PostList() {
     <>
       {posts.length > 0 && (
         <ul className={classes.posts}>
-          {posts.map((post, i) => (
-            <Post author={post.author} key={i} body={post.body} />
+          {posts.map((post) => (
+            <Post
+              author={post.author}
+              key={post.id}
+              id={post.id}
+              body={post.body}
+            />
           ))}
         </ul>
       )}
--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,9 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import RootLayout from "./routes/RootLayout.jsx";
 import Posts, { loader as postLoader } from "./routes/Posts.jsx";
 import NewPost, { action as newActionPost } from "./routes/NewPost.jsx";
-
+import PostDetails, {
+  loader as postDetailLoader,
+} from "./routes/PostDetails.jsx";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -18,6 +20,11 @@ const router = createBrowserRouter([
         loader: postLoader,
         children: [
           { path: "/create-post", element: <NewPost />, action: newActionPost },
+          {
+            path: "/:postId",
+            element: <PostDetails />,
+            loader: postDetailLoader,
+          },
         ],
       },
     ],
